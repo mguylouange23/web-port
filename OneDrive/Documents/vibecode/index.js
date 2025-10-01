d7869d40fae6a02d1619fa961ddd91b01eaaf57a
mguylouange23 <mguylouange23@gmail.com>
2025-10-01 17:48:43 +0200

    first commit

---
+++ OneDrive/Documents/vibecode/index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+            strings:["developers","founders","tech enthuasiasts"],
+            typeSpeed: 100,
+            backSpeed: 100,
+            backDelay: 100,
+            loop: true
+        });
